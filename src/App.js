2405f9daae7a162a79343e4b76bf6a24432cd463
noordseoostende <voropayev1@mail.ru>
2020-11-22 18:34:21 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return ( 
+    <>
+  <h1>Welcome to React Parcel Micro App!</h1>
+  <p>Hard to get more</p>
+  <h3>For you</h3>
+    </>
+  );
+};
+
+export default App;
